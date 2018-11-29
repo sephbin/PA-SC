fbbb02d935ef3dec929b3aa876445b9581d5f729
Andrew Butler <sephbin@gmail.com>
2018-11-30 00:41:35 +1100

    Update views.py

--- strangercollective/tests/views.py
+++ strangercollective/tests/views.py
@@ -10,4 +10,4 @@ def ortho(request):
 
 def dir(request):
 	# return HttpResponse(os.path.dirname(os.path.abspath(__file__)))
-	return HttpResponse(settings.STATICFILES_DIRS[1])
+	return HttpResponse(settings.STATICFILES_DIRS)

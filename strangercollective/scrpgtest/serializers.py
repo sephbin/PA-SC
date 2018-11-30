2b4811286e7f93ed8f4b65dd7ecc8c4915ba1784
Andrew Butler <andrew.butler@cox.com.au>
2018-11-30 18:04:58 +1100

    Update strangercollective/scrpgtest/serializers.py

--- strangercollective/scrpgtest/serializers.py
+++ strangercollective/scrpgtest/serializers.py
@@ -1,20 +1,7 @@
-from django.contrib.auth.models import User, Group
-from rest_framework import serializers
 from .models import *
+from rest_framework import serializers
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ('url', 'username', 'email', 'groups')
-
-
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Group
-        fields = ('url', 'name')
-
-class CharacterSerializer(serializers.HyperlinkedModelSerializer):
-	class Meta:
-		model = character
-		fields = ('url', 'firstname', 'lastname', 'occupation',)
+class CharacterSerializer(serializers.Serializer):
+	id = serializers.IntegerField(read_only=True)
+	firstname = serializers.CharField(required=False, allow_blank=True, max_length=100)

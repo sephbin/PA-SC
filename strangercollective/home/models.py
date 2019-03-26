601e72e0192d3a7a8643dba35ac1f3c2dac85a53
Andrew Butler <andrew.butler@cox.com.au>
2019-03-26 22:00:31 +1100

    Wagtail Tests

--- strangercollective/home/models.py
+++ strangercollective/home/models.py
@@ -2,25 +2,26 @@ from django.db import models
 
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel, InlinePanel
 from wagtail.search import index
+from wagtail.images.edit_handlers import ImageChooserPanel
 
 
 class HomePage(Page):
-    body = RichTextField(blank=True)
+	body = RichTextField(blank=True)
 
-    content_panels = Page.content_panels + [
-        FieldPanel('body', classname="full"),
-    ]
+	content_panels = Page.content_panels + [
+		FieldPanel('body', classname="full"),
+	]
 
 
 
 class BlogIndexPage(Page):
-    intro = RichTextField(blank=True)
+	intro = RichTextField(blank=True)
 
-    content_panels = Page.content_panels + [
-        FieldPanel('intro', classname="full")
-    ]
+	content_panels = Page.content_panels + [
+		FieldPanel('intro', classname="full")
+	]
 
 
 
@@ -28,36 +29,57 @@ class BlogIndexPage(Page):
 
 
 class BlogPage(Page):
-    date = models.DateField("Post date")
-    intro = models.CharField(max_length=250)
-    body = RichTextField(blank=True)
+	date = models.DateField("Post date")
+	intro = models.CharField(max_length=250)
+	body = RichTextField(blank=True)
 
-    search_fields = Page.search_fields + [
-        index.SearchField('intro'),
-        index.SearchField('body'),
-    ]
+	search_fields = Page.search_fields + [
+		index.SearchField('intro'),
+		index.SearchField('body'),
+	]
 
-    content_panels = Page.content_panels + [
-        FieldPanel('date'),
-        FieldPanel('intro'),
-        FieldPanel('body', classname="full"),
-    ]
+	content_panels = Page.content_panels + [
+		FieldPanel('date'),
+		FieldPanel('intro'),
+		FieldPanel('body', classname="full"),
+	]
 
 
 class AdvantageIndexPage(Page):
-    intro = RichTextField(blank=True)
+	intro = RichTextField(blank=True)
 
-    content_panels = Page.content_panels + [
-        FieldPanel('intro', classname="full")
-    ]
+	content_panels = Page.content_panels + [
+		FieldPanel('intro', classname="full")
+	]
 
 class AdvantagePage(Page):
 	body = RichTextField(blank=True)
-	
+	special_limitations = RichTextField(blank=True)
+	special_enhancements = RichTextField(blank=True)
+	feed_image = models.ForeignKey(
+		'wagtailimages.Image',
+		null=True,
+		blank=True,
+		on_delete=models.SET_NULL,
+		related_name='+'
+	)
+	exotic = models.BooleanField(default=False)
+	physical = models.BooleanField(default=False)
+
+
+
 	search_fields = Page.search_fields + [
-        index.SearchField('body'),
-    ]
+		index.SearchField('body'),
+	]
 
 	content_panels = Page.content_panels + [
+	MultiFieldPanel([FieldPanel('exotic'),FieldPanel('physical')], heading="Types", classname="collapsible collapsed"),
 	FieldPanel('body', classname="full"),
+	FieldPanel('special_limitations', classname="full"),
+	FieldPanel('special_enhancements', classname="full"),
+	]
+
+	promote_panels = [
+		MultiFieldPanel(Page.promote_panels, "Common page configuration"),
+		ImageChooserPanel('feed_image'),
 	]

27fd1d55c568ae1bf9a6d06d09943d6f5112a8d3
Andrew Butler <sephbin@gmail.com>
2019-06-21 22:47:31 +1000

    Update settings.py

--- strangercollective/strangercollective/settings.py
+++ strangercollective/strangercollective/settings.py
@@ -154,7 +154,7 @@ STATIC_URL = '/static/'
 # STATIC_URL = ''
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATICFILES_DIRS = [
-  # os.path.join(BASE_DIR, 'static/'),
+  os.path.join(BASE_DIR, 'static/'),
   os.path.join(BASE_DIR, 'crowbar/build/static'),
 ]
 

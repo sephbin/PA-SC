13b4cd4c372647962efcc18b03e5aa0899165070
Andrew Butler <sephbin@gmail.com>
2019-12-01 23:12:43 +1100

    Update admin.py

--- strangercollective/unswcode/admin.py
+++ strangercollective/unswcode/admin.py
@@ -9,7 +9,7 @@ class ListAdmin(admin.ModelAdmin):
 class testresultAdmind(admin.ModelAdmin):
 	list_display = ("test", "identifier", "question", "notes", "date", "score", "ip", "pcusername",)
 	list_filter = ("test","identifier",)
-	search_fields = ["identifier","pcusername","ip",'notes',]
+	search_fields = ["identifier","pcusername","ip",'notes','question',]
 
 class testQuestionAdmin(admin.ModelAdmin):
 	list_display = ("questionName", "questionText", "questionHint", "_archjson",)

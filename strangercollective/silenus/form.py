ac15025951b98e608de964ca8b730c8deed7447c
Andrew Butler <sephbin@gmail.com>
2022-07-02 18:39:54 +1000

    remove silenus

--- strangercollective/silenus/form.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm, Form
-from django.forms import DateField, CharField, ChoiceField, TextInput
-
-
-class masterAdvancedSearch(Form):
-	name = CharField(required=False)
-	identifier = CharField(required=False)
-	data = CharField(required=False)

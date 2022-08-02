10d25dab84d111095dfa81fa1298f93cc1afc15f
Andrew Butler <sephbin@gmail.com>
2022-08-02 12:20:42 +1000

    Update views.py

--- strangercollective/mimir/views.py
+++ strangercollective/mimir/views.py
@@ -5,40 +5,44 @@ from django.conf import settings
 from .models import *
 
 def index(request, path=None):
-	import requests
-	import markdown
-	import re, glob, os
-	from git import Repo
+	log = []
+	try:
+		import requests
+		import markdown
+		import re, glob, os
+		from git import Repo
 
-	mediaPath = settings.MEDIA_ROOT
-	subpath = os.path.join(mediaPath,"mimir","1")
-	try:	Repo.clone_from("https://github.com/sephbin/SyCoDe_Scribe", subpath)
-	except Exception as e: print(e)
-	md_files = glob.glob(subpath + "\**\*.md", recursive = True)
-	searchFile = "The Sublime Run of the Tangerine Oroborus.md"
-	if path:
-		searchFile = path
-	md_files = list(filter(lambda x:x.endswith(searchFile), md_files))
-	with open(md_files[0],"r") as file:
-		mdtext = file.read()
-	
-	# \[{2}(.+?)\]{2}
-	search = re.finditer(r"\[{2}(.+?)\]{2}", mdtext)
-	for s in search:
-		print(s)
-		print(s.group(1))
-		fromString = s.group(1)
-		try:	toString = fromString.split("|")[1]
-		except:	toString = fromString.split("|")[0]
-		# toString = fromString.replace("[[","[").replace("]]","]")
-		toLink = fromString.split("|")[0]
-		fromString = "[[%s]]"%(fromString)
-		toString = "[%s]"%(toString)
-		toLink = "(%s.md)"%(toLink)
-		mdtext = mdtext.replace(fromString,toString+toLink)
-	mdtext = mdtext.replace("\n","\n\n")
-	html = markdown.markdown(mdtext)
+		mediaPath = settings.MEDIA_ROOT
+		subpath = os.path.join(mediaPath,"mimir","1")
+		try:	Repo.clone_from("https://github.com/sephbin/SyCoDe_Scribe", subpath)
+		except Exception as e: print(e)
+		md_files = glob.glob(subpath + "\**\*.md", recursive = True)
+		searchFile = "README.md"
+		if path:
+			searchFile = path
+		md_files = list(filter(lambda x:x.endswith(searchFile), md_files))
+		with open(md_files[0],"r") as file:
+			mdtext = file.read()
+		
+		# \[{2}(.+?)\]{2}
+		search = re.finditer(r"\[{2}(.+?)\]{2}", mdtext)
+		for s in search:
+			print(s)
+			print(s.group(1))
+			fromString = s.group(1)
+			try:	toString = fromString.split("|")[1]
+			except:	toString = fromString.split("|")[0]
+			# toString = fromString.replace("[[","[").replace("]]","]")
+			toLink = fromString.split("|")[0]
+			fromString = "[[%s]]"%(fromString)
+			toString = "[%s]"%(toString)
+			toLink = "(%s.md)"%(toLink)
+			mdtext = mdtext.replace(fromString,toString+toLink)
+		mdtext = mdtext.replace("\n","\n\n")
+		html = markdown.markdown(mdtext)
 
-	# https://sephbin.github.io/SyCoDe_Scribe/
-	# https://raw.githubusercontent.com/sephbin/SyCoDe_Scribe/main/README.md
-	return HttpResponse(html)
+		# https://sephbin.github.io/SyCoDe_Scribe/
+		# https://raw.githubusercontent.com/sephbin/SyCoDe_Scribe/main/README.md
+		return HttpResponse(html)
+	except Exception as e:
+		return JsonResponse({"log":log, "error":str(e)})
